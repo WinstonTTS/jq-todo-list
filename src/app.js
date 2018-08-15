a37de797b6ccd94db53792d13ba96617ce218ab1
Blake Guilloud <blake.guilloud@gmail.com>
2018-08-14 20:42:04 -0400

    Remove unused.

--- src/app.js
+++ src/app.js
@@ -23,22 +23,3 @@ function toggleComplete() {
 function removeTodo() {
   this.remove()
 }
-
-
-$('#myButton').on('click', function(event){
-	//Update button html
-	$(this).html("button clicked");
-
-	//Create a new element
-	var newEl = $('<a>look at me!</a>');
-
-	//Append new element
-	$('#someOtherElement').append(newEl);
-
-	//Add an attribute
-	$('#someOtherElement').append(newEl).attr({'href' : 'http://techtalentsouth.com'})
-
-	//Remove it
-	newEl.remove();
-
-})
